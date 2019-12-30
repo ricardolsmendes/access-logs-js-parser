6a75da424b1334e699f1695bf83e08e8083308e4
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-12-30 19:44:09 -0300

    Allow public access to all attributes

--- src/log-data.js
+++ src/log-data.js
@@ -15,31 +15,15 @@ class LogData {
   constructor(
     remoteHost, remoteUser, datetime, request, httpStatus, bytesSent, httpReferer, userAgent) {
 
-    this._remoteHost = remoteHost;
-    this._remoteUser = remoteUser;
-    this._datetime = datetime;
-    this._request = request;
-    this._httpStatus = httpStatus;
-    this._bytesSent = bytesSent;
-    this._httpReferer = httpReferer;
-    this._userAgent = userAgent;
+    this.remoteHost = remoteHost;
+    this.remoteUser = remoteUser;
+    this.datetime = datetime;
+    this.request = request;
+    this.httpStatus = httpStatus;
+    this.bytesSent = bytesSent;
+    this.httpReferer = httpReferer;
+    this.userAgent = userAgent;
   }
-
-  get remoteHost() { return this._remoteHost; }
-
-  get remoteUser() { return this._remoteUser; }
-
-  get datetime() { return this._datetime; }
-
-  get request() { return this._request; }
-
-  get httpStatus() { return this._httpStatus; }
-
-  get bytesSent() { return this._bytesSent; }
-
-  get httpReferer() { return this._httpReferer; }
-
-  get userAgent() { return this._userAgent; }
 
 }
 
